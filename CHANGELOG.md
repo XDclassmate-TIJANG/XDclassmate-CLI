950c815cee9d9244de65f40150772ee042167256
XDclassmate-TIJANG <xdclassmate@outlook.com>
2026-08-29 18:25:31 +0800

    chore: 添加 CLI 更新检测清单与缓存头（CF Pages）

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-> 注释：此文档在**Alpha 1.10**开始维护，此前的修改见Commits。
-
-## Alpha 1.10
--- core/builtins/__init__.py
+++
@@ -1,4 +0,0 @@
-"""内置能力包：随微内核一起装载的最小功能集合。"""
-from .system_commands import register_system_commands
-
-__all__ = ["register_system_commands"]
